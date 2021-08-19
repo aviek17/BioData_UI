29514bd4a319314773abc40b84fc7e2adf3bc65c
Aviek <aviek17@gmail.com>
2021-08-20 01:09:28 +0530

    Application File

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { createContext, useReducer} from 'react'
+import "./App.css"
+import Navbar from './Components/Navbar'
+import Home from './Components/Home'
+import About from './Components/AboutUs'
+import Contact from './Components/Contact'
+import LogIn from './Components/LogIn'
+import SignUp from './Components/SignUp'
+import Error from "./Components/Error"
+import Logout from "./Components/Logout"
+import { Route, Switch } from 'react-router-dom'
+import {initialState,reducer} from '../src/Reducer/UseReducer'
+import 'bootstrap/dist/css/bootstrap.css'
+
+export const UserContext = createContext();
+
+const App = () => {
+  const [state, dispatch] = useReducer(reducer, initialState)
+  // context api
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <UserContext.Provider value={{state,dispatch}}>
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/login">
+            <LogIn />
+          </Route>
+          <Route path="/signup">
+            <SignUp />
+          </Route>
+          <Route path="/logout">
+            <Logout />
+          </Route>
+          <Route>
+            <Error />
+          </Route>
+        </Switch>
+      </UserContext.Provider>
+
+    </>
+  )
 }
 
-export default App;
+export default App
